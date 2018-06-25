d238d87aef791717198391e1bbe800816a095ea3
mc3747 <gjfax@macheng.local>
2018-06-25 09:57:18 +0800

    180625_添加轮播器和collectionView

--- ToolAndExtensionSummary/ToolAndExtensionSummary/ViewController.m
+++ ToolAndExtensionSummary/ToolAndExtensionSummary/ViewController.m
@@ -11,6 +11,11 @@
 #import "CustomBannerScrollView.h"
 #import "UIButton+Category.h"
 #import "BannerDemoVC.h"
+#import "SDViewController.h"
+#import "LeftViewController.h"
+#import "Demo1ViewController.h"
+#import "RightViewController.h"
+#import "SWRevealViewController.h"
 
 @interface ViewController ()
 
@@ -23,9 +28,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
   
-//    [self test1];
-//    [self test2];
-    [_testBtn1 addTarget:self action:@selector(test3) forControlEvents:UIControlEventTouchUpInside];
+    [_testBtn1 addTarget:self action:@selector(test5) forControlEvents:UIControlEventTouchUpInside];
 }
 #pragma mark -   按钮扩大点击范围
 - (void)test1 {
@@ -35,15 +38,48 @@
     [testBtn enlargeClickAreaWithGap:20];
     [self.view addSubview:testBtn];
 }
+
 #pragma mark -   普通滚动图片
 - (void)test2 {
         CustomBannerScrollView *bannerView = [[CustomBannerScrollView alloc] initWithFrame:CGRectMake(0, 100, MAIN_SCREEN_WIDTH, 250) ];
         [bannerView configPictures:@[@"home_ad_1",@"home_ad_2",@"home_ad_3"]];
         [self.view addSubview:bannerView];
 }
-#pragma mark -  复杂滚动
+#pragma mark -  循环滚动
 - (void)test3 {
+    SDViewController *vc = [[SDViewController alloc] init];
+    [self.navigationController pushViewController:vc animated:YES];
+}
+
+#pragma mark -  复杂滚动
+- (void)test4 {
     BannerDemoVC *vc = [[BannerDemoVC alloc] init];
     [self.navigationController pushViewController:vc animated:YES];
+}
+
+#pragma mark -  collectionView
+- (void)test5 {
+    
+    //左侧菜单栏
+    LeftViewController *leftViewController = [[LeftViewController alloc] init];
+    
+    //首页
+    Demo1ViewController *centerView1Controller = [[Demo1ViewController alloc] init];
+    
+    //右侧菜单栏
+    RightViewController *rightViewController = [[RightViewController alloc] init];
+    
+    SWRevealViewController *revealViewController = [[SWRevealViewController alloc] initWithRearViewController:leftViewController frontViewController:centerView1Controller];
+    revealViewController.rightViewController = rightViewController;
+    
+    //浮动层离左边距的宽度
+    revealViewController.rearViewRevealWidth = 230;
+    //    revealViewController.rightViewRevealWidth = 230;
+    
+    //是否让浮动层弹回原位
+    //mainRevealController.bounceBackOnOverdraw = NO;
+    [revealViewController setFrontViewPosition:FrontViewPositionLeft animated:YES];
+    
+    [self.navigationController pushViewController:revealViewController animated:YES];
 }
 @end

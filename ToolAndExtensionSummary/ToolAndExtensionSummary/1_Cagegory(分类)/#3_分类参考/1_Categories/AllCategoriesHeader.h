4d6748ec3a1c18b0e3918d3da2e6e020ba755029
mc3747 <gjfax@macheng.local>
2018-07-20 14:43:27 +0800

    180720-添加分类

---
+++ ToolAndExtensionSummary/ToolAndExtensionSummary/1_Cagegory(分类)/#3_分类参考/1_Categories/AllCategoriesHeader.h
@@ -0,0 +1,23 @@
+//
+//  AllCategoriesHeader.h
+//  https://github.com/slq0378/SLQCategories
+//
+//  Created by Christian on 16/9/12.
+//  Copyright © 2016年 MrSong. All rights reserved.
+//  各种好用分类方法
+
+#ifndef AllCategoriesHeader_h
+#define AllCategoriesHeader_h
+#import "UIImageCateogriesHeader.h"
+#import "UIButtonCategoriesHeader.h"
+#import "UIControl+Block.h"
+#import "UIDevice+Hardware.h"
+#import "UIColorCategoriesHeader.h"
+#import "UIBezierPath+BasicShapes.h"
+#import "UIBezierPath+SVG.h"
+
+#import "UIBezierPath+Symbol.h"
+#import "UIAlertView+Block.h"
+#import "UIApplication+ApplicationSize.h"
+#import "UIApplication+Permissions.h"
+#endif /* AllCategoriesHeader_h */
---
+++ ToolAndExtensionSummary/ToolAndExtensionSummary/1_Cagegory(分类)/#3_分类参考/1_Categories/UIImage/UIImageCateogriesHeader.h
@@ -0,0 +1,24 @@
+//
+//  UIImageCateogriesHeader_h
+//  https://github.com/slq0378/SLQCategories
+//
+//  Created by Christian on 16/9/9.
+//  Copyright © 2016年 MrSong. All rights reserved.
+//  
+
+#ifndef UIImageCateogriesHeader_h
+#define UIImageCateogriesHeader_h
+#import "UIImage+fixOrientation.h"
+#import "UIImage+Color.h"
+#import "UIImage+Blur.h"
+#import "UIImage+Alpha.h"
+#import "UIImage+Resize.h"
+#import "UIImage+RoundImage.h"
+#import "UIImage+Capture.h"
+#import "UIImage+Water.h"
+#import "UIImage+Gif.h"
+#import "UIImage+Rotate.h"
+#import "UIImage+Convert.h"
+#import "UIImage+CIImage.h"
+
+#endif /* UIImageCateogriesHeader_h */
